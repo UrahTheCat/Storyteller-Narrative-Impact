870dd2352d814622943c219bd5fd53aa7bb74aec
Urah <URAHCAT@GMAIL.COM>
2024-08-30 13:21:08 +0200

    Start of second part
    added game selection in menu with shift + 1/2
    added cheat menu (ctrl + shift + space)
    added second player character
    created second level scene

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,19 +1,49 @@
 extends Control
 
 @onready var start: Button = $HBoxContainer/Start
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var game_select = 0
+var cheats_enabled = false
 
 func _ready() -> void:
 	start.grab_focus()
 	RenderingServer.set_default_clear_color(Color.BLACK);
+	sprite_2d.hide()
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+	
+	if Input.is_action_just_pressed("selection_1"):
+		game_select = 1
+		print(game_select)
+		sprite_2d.set_frame(1)
+	elif Input.is_action_just_pressed("selection_2"):
+		game_select = 2
+		print(game_select)
+		sprite_2d.set_frame(2)
+	elif Input.is_action_just_pressed("selection_0"):
+		game_select = 0
+		print(game_select)
+		sprite_2d.set_frame(0)
+		
+	if Input.is_action_just_pressed("cheats"): 
+		if !cheats_enabled:
+			sprite_2d.show()
+			cheats_enabled = true
+		else: 
+			sprite_2d.hide()
+			cheats_enabled = false
 
 func _on_start_pressed() -> void:
-	var game_select = randi_range(1,2)
+	
 	print(game_select)
-	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+	if game_select == 1:
+		get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+	elif game_select ==2:
+		get_tree().change_scene_to_file("res://Scenes/Levels/level_no_story.tscn")
+	
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
